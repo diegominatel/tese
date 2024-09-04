0e7a44341eb67d796e76aec130a31f960f849b2e
diegominatel <38727569+diegominatel@users.noreply.github.com>
2024-09-03 21:12:55 -0300

    Add files via upload

---
+++ material-auxiliar/analise-tempo/mirt.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+''' General packages '''
+import pandas as pd
+
+''' Load the methods to use R '''
+import rpy2.robjects as ro
+from rpy2.robjects.packages import importr
+from rpy2.robjects import pandas2ri
+from rpy2.robjects.conversion import localconverter
+
+''' Load the 'mirt' package in R '''
+mirt_package = importr('mirt')
+mirt_package.mirtCluster()
+
+class Mirt:
+    '''
+    Class that runs the mirt package in R
+    '''
+    
+    def __init__(self):
+        self.__model = None
+        
+    def mirt_config(self, itemtype):
+        '''
+        Returns the parameters used in mirt
+        '''
+        technical = ro.r('list(NCYCLES = 500, message=FALSE, warn=FALSE)') 
+        params = dict() 
+        params['technical'] = technical
+        params['itemtype'] = itemtype
+        params['verbose'] = True
+        params['SE'] = True
+        
+        return params
+        
+    def calculate(self, item_modeling, itemtype):
+        '''
+        Args:
+                item_modeling:  (pd.DataFrame) where rows indicate individuals and columns indicate items, each value pair i,j 
+                                represents whether individual i correctly answered item j.
+        '''
+        item_names = list(item_modeling.columns)
+        ''' Convert DataFrame to R Frame '''
+        with localconverter(ro.default_converter + pandas2ri.converter):
+            item_modeling = ro.conversion.py2rpy(item_modeling)
+        ''' Convert List to R Vector '''    
+        #itemtype = ro.vectors.StrVector(itemtype)
+        ''' Run mirt '''
+        self.__model = mirt_package.mirt(item_modeling, 1, **self.mirt_config(itemtype))
+        #ability = mirt_package.fscores(self.__model)
+        
